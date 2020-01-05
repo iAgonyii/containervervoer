f85e110c398a7ec454b7bd03fcf54261434151aa
Roy Teneij <411850@student.fontys.nl>
2020-01-06 00:14:40 +0100

    Revert "local changes"
    This reverts commit a6173733508d6c2b34efe2936ec2a5099c8a5ce2.

---
+++ Containervervoer/Logic/Objects/Container/IContainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Containervervoer.Logic.Objects
+{
+    public interface IContainer
+    {
+        bool allowTopStack { get; }
+        int maxTopStackWeight { get; }
+        int weight { get; set; }
+        int maxWeight { get; }
+        List<Row> allowedRows { get; set; }
+    }
+}
